a4e0fc6c247cacfda8cca4bb92b515a4213645fc
Joao Victor <joviavelino@gmail.com>
2023-08-20 23:44:00 -0300

    sidebar finished + ajusts

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,7 @@ import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
  <div className='nav-bar'> 
@@ -10,16 +11,45 @@ const Sidebar = () => (
         <img clasname='sub-logo' src={'morte'} alt='joey' />
     </Link>
     <nav>
-        <NavLink exact='true' activeClassname='active' to='/'>
+        <NavLink exact='true' activeclassname='active' to='/'>
             <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
         </NavLink>
-        <NavLink exact='true' activeClassname='active' className='about-link' to='/about'>
+        <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
             <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
         </NavLink>
-        <NavLink exact='true' activeClassname='active' className='contact-link' to='/contact'>
+        <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact'>
             <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
         </NavLink>
     </nav>
+    <ul>
+        <li>
+            <a 
+                target='_blank' 
+                rel="noreferrer" 
+                href="https://www.linkedin.com/in/jo%C3%A3o-victor-avelino-magalh%C3%A3es-514742219/"
+            >
+                <FontAwesomeIcon icon={faLinkedin} color="$4d4d4e" />
+            </a>
+        </li>
+        <li>
+            <a 
+                target='_blank' 
+                rel="noreferrer" 
+                href="https://github.com/joaovavelino?tab=overview&from=2023-08-01&to=2023-08-21"
+            >
+                <FontAwesomeIcon icon={faGithub} color="$4d4d4e" />
+            </a>
+        </li>
+        <li>
+            <a 
+                target='_blank' 
+                rel="noreferrer" 
+                href="https://acrobat.adobe.com/id/urn:aaid:sc:US:f5f4efef-838a-41a7-8ebc-3c62f1302692"
+            >
+                <FontAwesomeIcon icon={faEnvelope} color="$4d4d4e" />
+            </a>
+        </li>
+    </ul>
  </div>)
 
 export default Sidebar
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
